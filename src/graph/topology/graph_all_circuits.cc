f92781d22947405daa2f8f4287d0fdeca284efe4
Tiago de Paula Peixoto <tiago@skewed.de>
2021-01-23 19:45:41 +0100

    all_circuits(): change from recursive to stack-based implementation
    This removes a stack overflow bug.

--- src/graph/topology/graph_all_circuits.cc
+++ src/graph/topology/graph_all_circuits.cc
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include <boost/graph/hawick_circuits.hpp>
+#include "boost/graph/hawick_circuits.hpp"
 
 #include "graph_tool.hh"
 #include "numpy_bind.hh"
